d4d6cda269288b00f18fd27a15a1ad7680764e42
K <kad9603@gmail.com>
2021-01-29 14:54:53 -0500

    created navbarsview and aboutview

---
+++ src/Views/NavBarsView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class NavBarsView extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Nav Bars</h1>
+            </div>
+        );
+    }
+}
